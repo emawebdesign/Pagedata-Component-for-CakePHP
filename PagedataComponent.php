77042b36b4475c26bd8f1d737d6978e36fc3f570
emawebdesign <emanuele.cali@gmail.com>
2017-04-24 19:42:25 +0200

    Update PagedataComponent.php

--- PagedataComponent.php
+++ PagedataComponent.php
@@ -11,22 +11,27 @@ class PagedataComponent extends Component {
 	
 	public function getTitle($url = NULL) {
 		
-		$html = file_get_contents($url);
+		if ($this->checkResponse($url)) {
 		
-		if (strlen($html)>0) {
+			$html = file_get_contents($url);
 			
-			$html = trim(preg_replace('/\s+/', ' ', $html));
-			preg_match("/\<title\>(.*)\<\/title\>/i",$html,$title);
+			if (strlen($html)>0) {
+				
+				$html = trim(preg_replace('/\s+/', ' ', $html));
+				preg_match("/\<title\>(.*)\<\/title\>/i",$html,$title);
+				
+				if (isset($title[1])) return $title[1];
+				else {
+					
+					$tags = get_meta_tags($url);
+					
+					if (isset($tags['title'])) return $tags['title'];
+					else return(false);
+					
+				}
 			
-			if (isset($title[1])) return $title[1];
-			else {
-				
-				$tags = get_meta_tags($url);
-				
-				if (isset($tags['title'])) return $tags['title'];
-				else return(false);
-				
 			}
+			else return(false);
 		
 		}
 		else return(false);
@@ -50,26 +55,31 @@ class PagedataComponent extends Component {
 	
 	public function getOpenGraph($url = NULL, $type = NULL) {
 
-		$data = file_get_contents($url);
-		$dom = new DomDocument;
-		@$dom->loadHTML($data);
-		 
-		$xpath = new DOMXPath($dom);
+		if ($this->checkResponse($url)) {
+			
+			$data = file_get_contents($url);
+			$dom = new DomDocument;
+			@$dom->loadHTML($data);
+			 
+			$xpath = new DOMXPath($dom);
+			
+			$metas = $xpath->query('//*/meta[starts-with(@property, \'og:\')]');
 		
-		$metas = $xpath->query('//*/meta[starts-with(@property, \'og:\')]');
+			$og = array();
+		
+			foreach($metas as $meta) {
+				
+				$property = str_replace('og:', '', $meta->getAttribute('property'));
 	
-		$og = array();
-	
-		foreach($metas as $meta) {
-			
-			$property = str_replace('og:', '', $meta->getAttribute('property'));
-
-			$content = $meta->getAttribute('content');
-			$og[$property] = $content;
-			
+				$content = $meta->getAttribute('content');
+				$og[$property] = $content;
+				
+			}
+		
+			if (isset($og[$type])) return $og[$type];
+			else return(false);
+		
 		}
-	
-		if (isset($og[$type])) return $og[$type];
 		else return(false);
 		
 	}
@@ -82,14 +92,19 @@ class PagedataComponent extends Component {
 		$allowedHtml = '<h1><h2><h3><h4><h5><h6><p><strong><a><br>';
 		if ($allowed_html!==NULL) $allowedHtml = $allowed_html;
 		
-		$html = file_get_contents($url);
-		
-		if (strlen($html)>0) {
+		if ($this->checkResponse($url)) {
 			
-			$html = trim(preg_replace('/\s+/', ' ', $html));
-			preg_match("/<body.*\/body>/s", $html, $matches);
+			$html = file_get_contents($url);
 			
-			if (isset($matches[0])) return(strip_tags($matches[0], $allowedHtml));
+			if (strlen($html)>0) {
+				
+				$html = trim(preg_replace('/\s+/', ' ', $html));
+				preg_match("/<body.*\/body>/s", $html, $matches);
+				
+				if (isset($matches[0])) return(strip_tags($matches[0], $allowedHtml));
+				else return(false);
+			
+			}
 			else return(false);
 		
 		}
@@ -101,27 +116,32 @@ class PagedataComponent extends Component {
 	
 	
 	public function getAllLinks($url = NULL) {
+		
+		if ($this->checkResponse($url)) {
 	
-		$html = @file_get_contents($url);
-
-		$dom = new DOMDocument;
-		
-		@$dom->loadHTML($html);
-		
-		$links = $dom->getElementsByTagName('a');
-		
-		$arr = array();
-		
-		foreach ($links as $link) {
+			$html = @file_get_contents($url);
+	
+			$dom = new DOMDocument;
 			
-			$arr[] = array(
-				'href' => $link->getAttribute('href'),
-				'anchor' => $link->nodeValue
-			);
+			@$dom->loadHTML($html);
 			
+			$links = $dom->getElementsByTagName('a');
+			
+			$arr = array();
+			
+			foreach ($links as $link) {
+				
+				$arr[] = array(
+					'href' => $link->getAttribute('href'),
+					'anchor' => $link->nodeValue
+				);
+				
+			}
+			
+			return($arr);
+		
 		}
-		
-		return($arr);
+		else return(false);
 		
 	}
 	
@@ -129,27 +149,32 @@ class PagedataComponent extends Component {
 	
 	
 	public function getAllImages($url = NULL) {
+		
+		if ($this->checkResponse($url)) {
 	
-		$html = @file_get_contents($url);
-
-		$dom = new DOMDocument;
-		
-		@$dom->loadHTML($html);
-		
-		$images = $dom->getElementsByTagName('img');
-		
-		$arr = array();
-		
-		foreach ($images as $image) {
+			$html = @file_get_contents($url);
+	
+			$dom = new DOMDocument;
 			
-			$arr[] = array(
-				'src' => $image->getAttribute('src'),
-				'alt' => $image->getAttribute('alt')
-			);
+			@$dom->loadHTML($html);
 			
+			$images = $dom->getElementsByTagName('img');
+			
+			$arr = array();
+			
+			foreach ($images as $image) {
+				
+				$arr[] = array(
+					'src' => $image->getAttribute('src'),
+					'alt' => $image->getAttribute('alt')
+				);
+				
+			}
+			
+			return($arr);
+		
 		}
-		
-		return($arr);
+		else return(false);
 		
 	}
 	
@@ -167,34 +192,53 @@ class PagedataComponent extends Component {
 		
 		$arr = array();
 		
-		$txt = strip_tags(file_get_contents($url));
-
-		$words = str_word_count(strtolower($txt),1);
-		$wcount = array_count_values($words);
+		if ($this->checkResponse($url)) {
 		
-		foreach ($wcount as $key=>$val) {
+			$txt = strip_tags(file_get_contents($url));
+	
+			$words = str_word_count(strtolower($txt),1);
+			$wcount = array_count_values($words);
 			
-			$density = ($val/count($words))*100;
-			
-			if (strlen($key)>3) {
+			foreach ($wcount as $key=>$val) {
 				
-				$arr[] = array(
-					'key' => $key,
-					'val' => $val,
-					'density' => number_format($density,2)
-				);
+				$density = ($val/count($words))*100;
+				
+				if (strlen($key)>3) {
+					
+					$arr[] = array(
+						'key' => $key,
+						'val' => $val,
+						'density' => number_format($density,2)
+					);
+					
+				}
 				
 			}
 			
+			usort($arr, function($a, $b) {
+				return $a['val'] - $b['val'];
+			});
+			
+			$arr = array_reverse($arr, true);
+			
+			return $arr;
+		
 		}
+		else return(false);
 		
-		usort($arr, function($a, $b) {
-			return $a['val'] - $b['val'];
-		});
+	}
+	
+	
+	
+	
+	public function checkResponse($url = NULL) {
 		
-		$arr = array_reverse($arr, true);
-		
-		return $arr;
+		if (@get_headers($url)) {
+			$headers = @get_headers($url);
+			if (strpos($headers[0], '200 OK') !== false) return(true);
+			else return(false);
+		}
+		else return(false);
 		
 	}
 	
